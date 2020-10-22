05be1ab44f951a509d6ae7cf6cdfb70ef95d9042
Ionel Tomi <ionel.tomi17@yahoo.com>
2020-10-22 22:00:49 +0300

    lab3

---
+++ Views/Books/Book.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tomi_Ionel_Lab2.Views.Books
+{
+    public class Book
+    {
+    }
+}
