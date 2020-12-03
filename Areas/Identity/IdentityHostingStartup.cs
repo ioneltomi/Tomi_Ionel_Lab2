ab6e30731a5c59ff4823a7033e95019ad0917bd3
Ionel Tomi <ionel.tomi17@yahoo.com>
2020-12-03 19:15:49 +0200

    Lab_9

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Tomi_Ionel_Lab2.Models;
+
+[assembly: HostingStartup(typeof(Tomi_Ionel_Lab2.Areas.Identity.IdentityHostingStartup))]
+namespace Tomi_Ionel_Lab2.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<IdentifyContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("IdentifyContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<IdentifyContext>();
+            });
+        }
+    }
+}
